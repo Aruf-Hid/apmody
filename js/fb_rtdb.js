148296d8aa603cbb0b49b362f4b9761a23e1dbea
ARUF <106890646+Aruf-Hid@users.noreply.github.com>
2022-06-09 20:47:56 +0700

    Update fb_rtdb.js

--- js/fb_rtdb.js
+++ js/fb_rtdb.js
@@ -9,7 +9,7 @@ ldJs("https://www.gstatic.com/firebasejs/8.9.1/firebase-app.js", "fb-ap", !0, "h
 
     if(ARtb.firebase.counter.enable==1){
     
-	    dD = qSell(".pvw");
+	    dD = qSell(".pApGm .iFxd,.pApGm .pAG");
 
 	    for(var dB=firebase.database(),n=0;n<dD.length;n++){var i=dD[n],dC=i.getAttribute("data-id");
 
@@ -28,6 +28,6 @@ ldJs("https://www.gstatic.com/firebasejs/8.9.1/firebase-app.js", "fb-ap", !0, "h
   });
 });
 
-if(1==ARtb.firebase.counter.enable)function _CvdAG(){dD=qSell(".pvw");for(var e=firebase.database(),a=0;a<dD.length;a++){var t=dD[a],n=t.getAttribute("data-id");(n=e.ref("BlogID_"+blogId+"/"+n)).once("value",function(e,a){return function(a){var t=a.exists()&&null!=a.val()._view?a.val()._view:0,n=a.exists()&&null!=a.val()._dl?a.val()._dl:0;e.classList.contains("s")||(n>0&&e.insertAdjacentHTML("afterbegin",'<span data-text="'+xAR.abv(n)+'">'+ARtb.firebase.counter.iconDl+"</span>"),t>0&&e.insertAdjacentHTML("afterbegin",'<span data-text="'+xAR.abv(t)+'">'+ARtb.firebase.counter.iconView+"</span>"),addCt(e,"s"))}}(t))}}
+if(1==ARtb.firebase.counter.enable)function _CvdAG(){dD=qSell(".pApGm .iFxd,.pApGm .pAG");for(var e=firebase.database(),a=0;a<dD.length;a++){var t=dD[a],n=t.getAttribute("data-id");(n=e.ref("BlogID_"+blogId+"/"+n)).once("value",function(e,a){return function(a){var t=a.exists()&&null!=a.val()._view?a.val()._view:0,n=a.exists()&&null!=a.val()._dl?a.val()._dl:0;e.classList.contains("s")||(n>0&&e.insertAdjacentHTML("afterbegin",'<span data-text="'+xAR.abv(n)+'">'+ARtb.firebase.counter.iconDl+"</span>"),t>0&&e.insertAdjacentHTML("afterbegin",'<span data-text="'+xAR.abv(t)+'">'+ARtb.firebase.counter.iconView+"</span>"),addCt(e,"s"))}}(t))}}
 }
 /*]]>*/
